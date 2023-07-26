13d65831ec790fbbd7a5c6367f7009ca3b1242cf
William José Sánchez Gutiérrez <williamjsg@gmail.com>
2023-07-26 17:32:30 -0600

    - Se crean endpoints para leer datos de csv - Se habilitan cors para permitir peticiones desde reactjs

--- services/cars_srv.py
+++ services/cars_srv.py
@@ -1,4 +1,18 @@
 import pandas as pd
+import csv
+
 def export_csv_to_parquet():
     df = pd.read_csv('./data/cars.csv')
     df.to_parquet('./temp/output.parquet')
+    
+def read_csv():
+    with open('./data/cars.csv') as f:
+        reader = csv.reader(f)
+        car_list = list(reader)
+    return car_list
+
+def data_from_csv():
+    with open("./data/cars.csv", "r") as file:
+        reader = csv.DictReader(file, delimiter=",")
+        data = list(reader)
+    return data    
